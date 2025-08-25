50ccebdb3ee7c2fdf47ae84b1dda3c34e2c1c201
Burke Strang <Burke.Strang@vertigis.com>
2025-08-25 10:21:00 -0400

    struct

--- arraysandhashing/containsduplicates.go
+++ arraysandhashing/containsduplicates.go
@@ -1,12 +1,12 @@
 package arraysandhashing
 
 func ContainsDuplicate(nums []int) bool {
-	unique := make(map[int]int)
+	unique := make(map[int]struct{})
 	for _, num := range nums {
 		if _, exists := unique[num]; exists {
 			return true
 		} else {
-			unique[num] = num
+			unique[num] = struct{}{}
 		}
 	}
 	return false
